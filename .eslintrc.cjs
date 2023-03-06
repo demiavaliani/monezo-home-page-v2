b1da15f45e29ab93c1cb34cdfeb80d1e13464363
demiavaliani <demikoavaliani@gmail.com>
2023-03-06 22:46:15 +0200

    chore: typescript setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,8 +7,12 @@ module.exports = {
 		'plugin:vue/vue3-essential',
 		'eslint:recommended',
 		'@vue/eslint-config-prettier/skip-formatting',
+		'plugin:@typescript-eslint/recommended',
 	],
+	parser: ['@typescript-eslint/parser', 'vue-eslint-parser'],
 	parserOptions: {
 		ecmaVersion: 'latest',
+		sourceType: 'module',
 	},
+	plugins: ['@typescript-eslint'],
 };
