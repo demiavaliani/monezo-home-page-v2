915aeda361e3d25fb6276ee8579ea9091d3aa533
demiavaliani <demikoavaliani@gmail.com>
2023-04-14 15:52:02 +0300

    fix: gsap scrolling different method

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,70 @@
 <template>
 	<div class="app-wrapper">
+		<FirstSection></FirstSection>
+		<SecondSection></SecondSection>
 		<ThirdSection></ThirdSection>
 	</div>
 </template>
 
 <script lang="ts">
 	import { defineComponent, onMounted } from 'vue';
+	import FirstSection from './views/FirstSection.vue';
+	import SecondSection from './views/SecondSection.vue';
 	import ThirdSection from './views/ThirdSection.vue';
 	import gsap from 'gsap';
-	import { ScrollTrigger } from 'gsap/ScrollTrigger';
+	import { Power2 } from 'gsap';
 	import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+	import { Observer } from 'gsap/Observer';
 
 	export default defineComponent({
-		components: { ThirdSection },
+		components: { FirstSection, SecondSection, ThirdSection },
 
 		setup() {
-			const goToSection = (section) => {
-				gsap.to(window, {
-					scrollTo: { y: section, autoKill: false },
-					overwrite: true,
-					duration: 1,
-				});
-			};
-
 			onMounted(() => {
-				gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+				gsap.registerPlugin(ScrollToPlugin, Observer);
+				const sections = gsap.utils.toArray('#snap-section');
+				let currentSection = sections[0];
+				let sectionToGo = sections[1];
+				let scrolling = false;
 
-				document.querySelectorAll('#snap-section').forEach((section) => {
-					ScrollTrigger.create({
-						trigger: section,
-						onEnter: () => {
-							if (section.classList.contains('first-section')) {
-								return;
-							}
-							goToSection(section);
-						},
-						onEnterBack: () => {
-							goToSection(section);
+				const goToSection = (section) => {
+					gsap.to(window, {
+						scrollTo: { y: section, autoKill: false },
+						overwrite: true,
+						duration: 0.2,
+						ease: Power2.easeOut,
+						onComplete: () => {
+							scrolling = true;
+							currentSection = sectionToGo;
+							setTimeout(() => {
+								scrolling = false;
+							}, 200);
 						},
 					});
+				};
+
+				Observer.create({
+					type: 'wheel,scroll',
+					onDown: () => {
+						if (!scrolling) {
+							sectionToGo = sections[sections.indexOf(currentSection) + 1];
+							if (sectionToGo) {
+								goToSection(sectionToGo);
+							}
+						}
+					},
+					onUp: () => {
+						if (!scrolling) {
+							sectionToGo = sections[sections.indexOf(currentSection) - 1];
+							if (sectionToGo) {
+								goToSection(sectionToGo);
+							}
+						}
+					},
+
+					preventDefault: true,
 				});
 			});
-
 			return {};
 		},
 	});
